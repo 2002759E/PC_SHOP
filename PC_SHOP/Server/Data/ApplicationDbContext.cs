16188abdc2f3bdbd300052040e987eb72833f6d4
ARGANA JOHN ROMEO BRIAN LATEO <2002759E@student.tp.edu.sg>
2022-01-26 19:43:19 +0800

    Add project files.

---
+++ PC_SHOP/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using PC_SHOP.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_SHOP.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}

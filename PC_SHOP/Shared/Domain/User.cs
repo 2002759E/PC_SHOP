859653db05062fda86a30a98f0007167f8002a4f
ALEXANDER TAN WEI EN 2001875J <2001875J@student.tp.edu.sg>
2022-01-27 10:30:40 +0800

    Added database and default data

---
+++ PC_SHOP/Shared/Domain/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PC_SHOP.Shared.Domain
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Title { get; set; }
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}

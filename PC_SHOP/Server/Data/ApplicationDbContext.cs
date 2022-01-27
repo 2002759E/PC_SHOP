b2ce1e74e7e8d3a35ed9fe15f853fdda78c42435
ALEXANDER TAN WEI EN 2001875J <2001875J@student.tp.edu.sg>
2022-01-27 12:48:36 +0800

    Added Seeds

--- PC_SHOP/Server/Data/ApplicationDbContext.cs
+++ PC_SHOP/Server/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using PC_SHOP.Server.Models;
+using PC_SHOP.Shared.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,5 +18,9 @@ namespace PC_SHOP.Server.Data
             IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
         {
         }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<PaymentType> Payments { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+
     }
 }
---
+++ PC_SHOP/Shared/Domain/PaymentType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PC_SHOP.Shared.Domain
+{
+    public class PaymentType
+    {
+        public int PaymentTypeID { get; set; }
+        public string Payment_Type { get; set; }
+    }
+}

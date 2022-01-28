b74e966c8337a58789a1626bb5e8f966677c6ce0
ARGANA JOHN ROMEO BRIAN LATEO <2002759E@student.tp.edu.sg>
2022-01-28 22:42:55 +0800

    item page

---
+++ PC_SHOP/Client/Static/Endpoints.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_SHOP.Client.Static
+{
+    public static class Endpoints
+    {
+        private static readonly string Prefix = "api";
+
+        public static readonly string BrandsEndpoint = $"{Prefix}/brands";
+        public static readonly string CategoriesEndpoint = $"{Prefix}/categories";
+        public static readonly string ItemsEndpoint = $"{Prefix}/items";
+        public static readonly string ListItemsEndpoint = $"{Prefix}/listitems";
+        public static readonly string OffersEndpoint = $"{Prefix}/offers";
+        public static readonly string PaymentsEndpoint = $"{Prefix}/payments";
+        public static readonly string RequestsEndpoint = $"{Prefix}/requests";
+        public static readonly string ReviewsEndpoint = $"{Prefix}/reviews";
+    }
+}
